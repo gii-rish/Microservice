b08d778c94e1f5785c3c7707d9a5d62fd2eb1e9d
Girish M <girish@bridgetree.com>
2021-06-26 18:55:18 +0530

    initial commit

---
+++ admin/products/views.py
@@ -0,0 +1,50 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Products, User
+from .serializers import ProductSerializer
+import random
+
+class ProductViewSet(viewsets.ViewSet):
+    
+    def list(self, request):
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+    def retrieve(self, request, pk=None):
+        product = Products.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+        
+    
+    def update(self, request, pk=None):
+        product = Products.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    
+    def destroy(self, request, pk=None):
+        product = Products.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
+class UserAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            "id": user.id
+        })
